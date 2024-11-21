a55318a6a262229c50eaa9e348e782465c13bd98
Abdul Kader <abdulkader0126@gmail.com>
2024-11-21 15:34:01 +0700

    update

--- Matematika.java
+++ Matematika.java
@@ -2,8 +2,8 @@ package matematika;
 
 public class Matematika {
 
-    public int pengurangan(int angka1, int angka2){
-        return angka1 - angka2;
+    public int perkalian(int angka1, int angka2){
+        return angka1 * angka2;
     }
 
     public  int pertambahan(int angka1, int angka2){
--- MatematikaCanggih.java
+++ MatematikaCanggih.java
@@ -8,18 +8,18 @@ public class MatematikaCanggih extends Matematika{
 
     public void pilihan(){
         System.out.println("\n=========Selamat datang di aplikasi matematika==========");
-        System.out.println("\n1. Pengurangan");
-        System.out.println("2. Pertambahan");
+        System.out.println("\n1. Pertambahan");
+        System.out.println("2. Perkalian");
         System.out.println("3. Modulus / Sisa bagi");
     }
 
     public void getpilihan(int pilihan , int angka1 , int angka2){
         switch (pilihan) {
             case 1:
-                System.out.println("\nHasil Pengurangan: " + angka1 + " - " + angka2 + " = " + pengurangan(angka1, angka2));
+                System.out.println("\nHasil Pertambahan: " + angka1 + " + " + angka2 + " = " + pertambahan(angka1, angka2));
                 break;
             case 2:
-                System.out.println("\nHasil Pertambahan: " + angka1 + " + " + angka2 + " = " + pertambahan(angka1, angka2));
+                System.out.println("\nHasil Perkalian: " + angka1 + " * " + angka2 + " = " + perkalian(angka1, angka2));
                 break;
             case 3:
                 if (angka2 == 0) {
