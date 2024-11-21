ab8d6ef9153b41c0ff67667f44a94569c88963ea
Abdul Kader <abdulkader0126@gmail.com>
2024-11-21 15:34:55 +0700

    Update Matematika.java

--- Matematika.java
+++ Matematika.java
@@ -2,12 +2,12 @@ package matematika;
 
 public class Matematika {
 
-    public int perkalian(int angka1, int angka2){
-        return angka1 * angka2;
-    }
-
     public  int pertambahan(int angka1, int angka2){
         return angka1 + angka2;
     }
-
+    
+    public int perkalian(int angka1, int angka2){
+        return angka1 * angka2;
+    }
+    
 }
